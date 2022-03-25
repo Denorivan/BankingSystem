146bd8a0dfec599b2c85afd0a17742ea60d88233
Nikita <nikita.gubarev31@gmail.com>
2022-03-25 17:30:44 +0300

    I added password and nik for person. And did getters for them

--- Banking_system/Person.h
+++ Banking_system/Person.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <iostream>
+#include <Windows.h>
+#include <ctime>
 #include <string>
+#include <vector>
 
 #include "BankAccount.h"
 //----------------------------------------------------------------------------------------------
@@ -8,14 +12,20 @@ using namespace std;
 //----------------------------------------------------------------------------------------------
 class Person {
 private:
-	string name, surname;
+	string name, surname, nik, password;
 	int age, count = 0;
 public:
 	int number;
+	vector<BankAccount> personAccs;
 
 	void enterName();
 	void enterSurname();
 	void enterAge();
+	void enterNik();
+	void enterPassword();
+
+	string getNik();
+	string getPassword();
 
 	void personInfo();
 	//------ Дружеский метод для перегрузки опертаора вывода ------
