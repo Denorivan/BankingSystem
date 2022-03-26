8878eb4d638df040038eae8512deafe9fdbef799
Nikita <nikita.gubarev31@gmail.com>
2022-03-26 18:10:06 +0300

    I added password for bank account and added getter for password

--- Banking_system/BankAccount.h
+++ Banking_system/BankAccount.h
@@ -10,14 +10,21 @@ using namespace std;
 class BankAccount
 {
 protected:
-	string id, number, owner, purpose;
+	string id, AccNumber, owner, purpose, password;
 	double money;
 public:
+	int number;
+
 	void enterOwner();
 	void enterPurpose();
+	void enterPassword();
+
+	string getPassword();
 
 	void idGeneration();     //Метод для генерации айди счёта
 	void numberGeneration(); //Метод для генерации номера счёта
 	void bankAccountInfo();  //Метод для вывода информации про счёт
+	//------ Дружеский метод для перегрузки опертаора вывода ------
+	friend ostream& operator<< (ostream& out, BankAccount& acc);
 };
 //----------------------------------------------------------------------------------------------
